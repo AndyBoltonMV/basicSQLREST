b59f0435519aa801ba0b824a9efc57c67a1fcab0
Andy Bolton <andrew.bolton@multiverse.io>
2022-08-22 21:55:57 +0100

    Added full CRUD in controllers for user model

---
+++ src/controllers/index.js
@@ -0,0 +1,15 @@
+const {
+  createUser,
+  readUser,
+  login,
+  updateUser,
+  deleteUser,
+} = require("./user");
+
+module.exports = {
+  createUser,
+  readUser,
+  login,
+  updateUser,
+  deleteUser,
+};
---
+++ src/controllers/user.js
@@ -0,0 +1,54 @@
+const jwt = require("jsonwebtoken");
+const { User } = require("../models");
+const { secret } = require("../../config");
+
+exports.createUser = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    const token = jwt.sign(user.id, secret);
+
+    res.status(200).send({ user, token });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+exports.readUser = async (req, res) => {
+  try {
+    const users = await User.findAll({ where: req.query });
+
+    res.status(200).send({ users });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const token = jwt.sign(req.user.id, secret);
+
+    res.status(200).send({ user: req.user, token });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+exports.updateUser = async (req, res) => {
+  try {
+    const updatedUser = await User.update(req.body, { where: req.user });
+
+    res.status(200).send({ updatedUser });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+    const deletedUser = await User.destroy(req.user);
+
+    res.status(200).send({ deletedUser });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
