e1b1d606096da119b1b79c7da7f0305f19eec304
Andy Bolton <andrew.bolton@multiverse.io>
2022-08-22 22:21:46 +0100

    Added exports to index.js

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -6,10 +6,23 @@ const {
   deleteUser,
 } = require("./user");
 
+const {
+  createContent,
+  readContent,
+  readByUser,
+  updateContent,
+  deleteContent,
+} = require("./content");
+
 module.exports = {
   createUser,
   readUser,
   login,
   updateUser,
   deleteUser,
+  createContent,
+  readContent,
+  readByUser,
+  updateContent,
+  deleteContent,
 };
